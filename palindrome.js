037a0516d843d26469f19c78764fb56a2ec80f34
Zak Slayback <zslayback@gmail.com>
2020-10-16 15:33:27 -0400

    Add palindrome.js updates using prototypes.

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,37 @@
-// Reverses a string.
-function reverse(string) {
-  return Array.from(string).reverse().join("");
+// Adds 'reverse' to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
 
-// Returns true for a palindrome, false otherwise.
-function palindrome(string) {
-  let processedContent = string.toLowerCase();
-  return processedContent === reverse(processedContent);
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Returns the lower-cased version of the content.
+  this.processor = function(string) {
+    return this.content.toLowerCase();
+  }
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
 }
+
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Returns translation processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.translation);
+  }
+}
+
+TranslatedPhrase.prototype = new Phrase();
