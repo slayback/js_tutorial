a33390c1ec1e602b1a6a5ec2be3d0e9efbf43722
Zak Slayback <zslayback@gmail.com>
2020-10-25 22:51:01 -0400

    Build wikipedia scraper shell command.

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 let Phrase = require("slayback-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string)
+function palindromeTester(event) {
+  event.preventDefault();
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
 
-if (phrase.palindrome())  {
-  alert(`"${phrase.content}" is a palindrome!`);
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`)
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> is a palindrome!`;
+  } else {
+    palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> is not a palindrome.`;
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit", function(event) {
+    palindromeTester(event);
+  });
+});
